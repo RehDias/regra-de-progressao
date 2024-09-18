41b3a097bde11a490143cf4454bb28addf3f27d5
renata dias <renata_dias96@live.com>
2024-09-18 12:02:45 -0300

    Criação do método result, que calcula a nota final e informa se é aprovado ou reprovado

--- src/main/java/com/trybe/java/regraprogressao/Progression.java
+++ src/main/java/com/trybe/java/regraprogressao/Progression.java
@@ -1,5 +1,6 @@
 package com.trybe.java.regraprogressao;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -34,14 +35,40 @@ public class Progression {
 
       qtdActivities -= 1;
       count += 1;
-    } while(qtdActivities > 0);
+    } while (qtdActivities > 0);
 
     int soma = weight.stream().reduce(0, Integer::sum);
 
     if (soma != 100) {
       System.out.println("A soma dos pesos é diferente de 100!");
+    } else {
+      result();
     }
 
     scanner.close();
+  }
+
+  /**
+   * Result.
+   */
+  public void result() {
+    ArrayList<Integer> weightPlusGrades = new ArrayList<>();
+
+    for (int index = 0; index < weight.size(); index += 1) {
+      weightPlusGrades.add(weight.get(index) * grades.get(index));
+    }
+
+    int sumGrade = weightPlusGrades.stream().reduce(0, Integer::sum);
+    double finalGrade = (double) sumGrade / 100;
+
+    if (finalGrade >= 85.0) {
+      System.out.println("Parabéns! Você alcançou " + finalGrade + "%!"
+          + " E temos o prazer de informar que você obteve aprovação!");
+    } else {
+      System.out.println("Lamentamos informar que, com base na sua pontuação alcançada "
+          + "neste período, " + finalGrade + "%, você não atingiu a pontuação mínima necessária "
+          + "para sua aprovação.");
+    }
+
   }
 }
